b0cbfa396e086259c4c3c0c4d111b3a4a2354407
Michael Park <thatmichaelpark@gmail.com>
2016-07-08 21:14:47 -0700

    add created_at and updated_at to seed file

--- seeds/1_people.js
+++ seeds/1_people.js
@@ -9,6 +9,8 @@ exports.seed = function(knex) {
         middle_name: 'Phuong',
         last_name: 'Dang',
         dob: new Date(1984, 11, 1),
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
@@ -17,6 +19,8 @@ exports.seed = function(knex) {
         middle_name: 'Thi',
         last_name: 'Dang',
         dob: new Date(1963, 3, 20),
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
@@ -25,6 +29,8 @@ exports.seed = function(knex) {
         middle_name: 'Hoai',
         last_name: 'Nguyen',
         dob: new Date(1995, 2, 5),
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'm'
       },
       {
@@ -32,6 +38,8 @@ exports.seed = function(knex) {
         first_name: 'Thach',
         middle_name: 'Van',
         last_name: 'Nguyen',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'm'
       },
       {
@@ -39,6 +47,8 @@ exports.seed = function(knex) {
         first_name: 'Lan',
         middle_name: 'Thi',
         last_name: 'Nguyen',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
@@ -46,66 +56,88 @@ exports.seed = function(knex) {
         first_name: 'Hoi',
         middle_name: 'Van',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'm'
       },
       {
         id: 7,
         first_name: 'Nhung',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 8,
         first_name: 'Hanh',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 9,
         first_name: 'Chi',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 10,
         first_name: 'Tu',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 11,
         first_name: 'Phuc',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 12,
         first_name: 'Thailand',
         last_name: 'Dang',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 13,
         first_name: 'Ricky',
         last_name: 'Saeturn',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'm'
       },
       {
         id: 14,
         first_name: 'Sidney',
         last_name: 'Saeturn',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 15,
         first_name: 'Lily',
         last_name: 'Saeturn',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
       },
       {
         id: 16,
         first_name: 'Nathan',
         last_name: 'Saeturn',
+        created_at: new Date("2016-07-08T03:33:59.857Z"),
+        updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'm'
       },
     ]);
