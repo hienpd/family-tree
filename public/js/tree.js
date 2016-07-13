eed7d6443d27bcefd1b38d78d16fd2a08720d895
Michael Park <thatmichaelpark@gmail.com>
2016-07-13 10:56:36 -0700

    get date picker and gender selector to work

--- public/js/tree.js
+++ public/js/tree.js
@@ -21,16 +21,64 @@ $xhr.done(function(data) {
     $xhr.done((person) => {
       let dob = 'n/a';
       if (person.dob) {
+        const twoDigit = function (d) {
+          return ('0' + d).slice(-2);
+        }
         const date = new Date(person.dob);
-        dob = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+        dob = date.getFullYear() + '-' + twoDigit(date.getMonth() + 1) + '-' + twoDigit(date.getDate());
       }
       $('.modal-content').empty();
       $('.modal-content').append($(
-        `<p>${person.given_name} ${person.middle_name} ${person.family_name}</p>
-        <p>${dob}</p>
-        <p>${person.gender}</p>
         `
+        <div class="row">
+          <div class="input-field col s4">
+            <input value="${person.given_name}" id="edit_given_name" type="text" class="validate">
+            <label class="active" for="edit_given_name">Given Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s4">
+            <input value="${person.middle_name}" id="edit_middle_name" type="text" class="validate">
+            <label class="active" for="edit_middle_name">Middle Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s4">
+            <input value="${person.family_name}" id="edit_family_name" type="text" class="validate">
+            <label class="active" for="edit_family_name">Family Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6">
+            <input type="date" value="${dob}" class="datepicker" id="edit_dob">
+            <label for="edit_dob" class="active">Date of Birth</label>
+          </div>
+          <div class="input-field col s6">
+            <select id="edit_gender">
+              <option value="" disabled selected>Gender</option>
+              <option value="f">Female</option>
+              <option value="m">Male</option>
+              <option value="a">Agender</option>
+              <option value="i">Intersex</option>
+              <option value="p">Pangender</option>
+              <option value="t">Transgender/Transsexual</option>
+              <option value="s">Two-Spirit</option>
+            </select>
+            <label>Gender</label>
+        </div>
+
+              `
       ));
+
+      $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 150, // Creates a dropdown of 15 years to control year
+        format: 'yyyy-mm-dd'
+      });
+      $(`#edit_gender option[value=${person.gender}]`).prop('selected', true);
+      Materialize.updateTextFields();
+      $('select').material_select();
+
       $('#modal1').openModal();
 
     });
