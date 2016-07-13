1bab5e59dd475d8c3f9c443fab136189dfed2f44
Hien Dang <hienpd@outlook.com>
2016-07-12 18:40:31 -0700

    Populate people on tree page in list

--- public/js/tree.js
+++ public/js/tree.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const $xhr = $.ajax({
+  method: 'GET',
+  url: '/people'
+});
+
+$xhr.done(function(data) {
+  Materialize.toast('success!', 4000);
+  for (const person of data) {
+    const name = `${person.given_name} ${person.middle_name} ${person.family_name}`;
+    $('#list').append($(`<li>${name}</li>`));
+  }
+
+});
+
+$xhr.fail(function(err) {
+  Materialize.toast('fail!', 4000);
+  console.err(err);
+});
+
+
+
 $('#logout').click((event) => {
   var $xhr = $.ajax({
     method: 'DELETE',
