9e3d3dfb998f4fe3eb2deb42ed36ff0a97c91361
Michael Park <thatmichaelpark@gmail.com>
2016-07-13 10:22:08 -0700

    populate edit modal with person info

--- public/js/tree.js
+++ public/js/tree.js
@@ -8,14 +8,37 @@ const $xhr = $.ajax({
 $xhr.done(function(data) {
   for (const person of data) {
     const name = `${person.given_name} ${person.middle_name} ${person.family_name}`;
-    $('#list').append($(`<li data-id="${person.id}">${name}</li>`));
+    $('#list').append($(`<li>${name} <a data-id="${person.id}">Edit</a></li>`));
   }
 
-  $('#list').on('click', 'li', (event) => {
+  $('#list').on('click', 'a', (event) => {
     const id = $(event.target).attr('data-id');
-    $('.modal-content').append($(`<h4>ID = ${id}</h4>`))
-    $('#modal1').openModal();
-  })
+    const $xhr = $.ajax({
+      method: 'GET',
+      url: '/people/' + id
+    });
+
+    $xhr.done((person) => {
+      let dob = 'n/a';
+      if (person.dob) {
+        const date = new Date(person.dob);
+        dob = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+      }
+      $('.modal-content').empty();
+      $('.modal-content').append($(
+        `<p>${person.given_name} ${person.middle_name} ${person.family_name}</p>
+        <p>${dob}</p>
+        <p>${person.gender}</p>
+        `
+      ));
+      $('#modal1').openModal();
+
+    });
+
+    $xhr.fail((err) => {
+      Materialize.toast('Unable to edit', 4000);
+    });
+  });
 
 });
 
