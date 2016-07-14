f328fc9cb59006492347335ff42f75a1c357ee1d
Hien Dang <hienpd@outlook.com>
2016-07-14 16:51:32 -0700

    New seed files

--- seeds/1_people.js
+++ seeds/1_people.js
@@ -5,143 +5,47 @@ exports.seed = function(knex) {
     .then(() => {
       return knex('people').insert([{
         id: 1,
-        given_name: 'Hien',
-        middle_name: 'Phuong',
-        family_name: 'Dang',
-        dob: new Date(1984, 11, 1),
+        given_name: 'Bob',
+        family_name: 'Belcher',
+        dob: new Date(1972, 2, 16),
         created_at: new Date("2016-07-08T03:33:59.857Z"),
         updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f',
-        user_id: 1
-      },
-      {
+        gender: 'm',
+      }, {
         id: 2,
-        given_name: 'Phung',
-        middle_name: 'Thi',
-        family_name: 'Dang',
-        dob: new Date(1963, 3, 20),
+        given_name: 'Linda',
+        family_name: 'Belcher',
+        dob: new Date(1972, 6, 24),
         created_at: new Date("2016-07-08T03:33:59.857Z"),
         updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
-      },
-      {
+      }, {
         id: 3,
-        given_name: 'Nicholas',
-        middle_name: 'Hoai',
-        family_name: 'Nguyen',
-        dob: new Date(1995, 2, 5),
+        given_name: 'Tina',
+        middle_name: 'Ruth',
+        family_name: 'Belcher',
+        dob: new Date(2003, 3, 5),
         created_at: new Date("2016-07-08T03:33:59.857Z"),
         updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'm'
-      },
-      {
+        gender: 'f'
+        user_id: 1
+      }, {
         id: 4,
-        given_name: 'Thach',
-        middle_name: 'Van',
-        family_name: 'Nguyen',
+        given_name: 'Gene',
+        family_name: 'Belcher',
+        dob: new Date(2005, 3, 5),
         created_at: new Date("2016-07-08T03:33:59.857Z"),
         updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'm'
-      },
-      {
+        gender: 'm',
+      }, {
         id: 5,
-        given_name: 'Lan',
-        middle_name: 'Thi',
-        family_name: 'Nguyen',
+        given_name: 'Louise',
+        family_name: 'Belcher',
+        dob: new Date(2007, 11, 25),
         created_at: new Date("2016-07-08T03:33:59.857Z"),
         updated_at: new Date("2016-07-08T03:33:59.857Z"),
         gender: 'f'
-      },
-      {
-        id: 6,
-        given_name: 'Hoi',
-        middle_name: 'Van',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'm'
-      },
-      {
-        id: 7,
-        given_name: 'Nhung',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 8,
-        given_name: 'Hanh',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 9,
-        given_name: 'Chi',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 10,
-        given_name: 'Tu',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 11,
-        given_name: 'Phuc',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 12,
-        given_name: 'Thailand',
-        family_name: 'Dang',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 13,
-        given_name: 'Ricky',
-        family_name: 'Saeturn',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'm'
-      },
-      {
-        id: 14,
-        given_name: 'Sidney',
-        family_name: 'Saeturn',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 15,
-        given_name: 'Lily',
-        family_name: 'Saeturn',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'f'
-      },
-      {
-        id: 16,
-        given_name: 'Nathan',
-        family_name: 'Saeturn',
-        created_at: new Date("2016-07-08T03:33:59.857Z"),
-        updated_at: new Date("2016-07-08T03:33:59.857Z"),
-        gender: 'm'
-      },
-    ]);
+      }]);
     })
     .then(() => {
       return knex.raw(
--- seeds/2_parents_children.js
+++ seeds/2_parents_children.js
@@ -5,125 +5,29 @@ exports.seed = function(knex) {
     .then(() => {
       return knex('parents_children').insert([{
         id: 1,
-        child_id: 1,
-        parent_id: 2
-      },
-      {
+        child_id: 3,
+        parent_id: 1
+      }, {
         id: 2,
-        child_id: 1,
-        parent_id: 4
-      },
-      {
-        id: 3,
-        child_id: 2,
-        parent_id: 5
-      },
-      {
-        id: 4,
-        child_id: 2,
-        parent_id: 16 // TEST
-      },
-      {
-        id: 5,
         child_id: 3,
         parent_id: 2
-      },
-      {
+      }, {
+        id: 3,
+        child_id: 4,
+        parent_id: 1
+      }, {
+        id: 4,
+        child_id: 4,
+        parent_id: 2
+      }, {
+        id: 5,
+        child_id: 5,
+        parent_id: 1
+      }, {
         id: 6,
-        child_id: 3,
-        parent_id: 4
-      },
-      {
-        id: 8,
-        child_id: 7,
-        parent_id: 5
-      },
-      {
-        id: 9,
-        child_id: 7,
-        parent_id: 6
-      },
-      {
-        id: 10,
-        child_id: 8,
-        parent_id: 5
-      },
-      {
-        id: 11,
-        child_id: 8,
-        parent_id: 6
-      },
-      {
-        id: 12,
-        child_id: 9,
-        parent_id: 5
-      },
-      {
-        id: 13,
-        child_id: 9,
-        parent_id: 6
-      },
-      {
-        id: 14,
-        child_id: 10,
-        parent_id: 5
-      },
-      {
-        id: 15,
-        child_id: 10,
-        parent_id: 6
-      },
-      {
-        id: 16,
-        child_id: 11,
-        parent_id: 5
-      },
-      {
-        id: 17,
-        child_id: 11,
-        parent_id: 6
-      }
-      //, {
-      //   id: 18,
-      //   child_id: 12,
-      //   parent_id: 5
-      // },
-      // {
-      //   id: 19,
-      //   child_id: 12,
-      //   parent_id: 6
-      // },
-      // {
-      //   id: 20,
-      //   child_id: 14,
-      //   parent_id: 12
-      // },
-      // {
-      //   id: 21,
-      //   child_id: 14,
-      //   parent_id: 13
-      // },
-      // {
-      //   id: 22,
-      //   child_id: 15,
-      //   parent_id: 12
-      // },
-      // {
-      //   id: 23,
-      //   child_id: 15,
-      //   parent_id: 13
-      // },
-      // {
-      //   id: 24,
-      //   child_id: 16,
-      //   parent_id: 12
-      // },
-      // {
-      //   id: 25,
-      //   child_id: 16,
-      //   parent_id: 13
-      // }
-    ]);
+        child_id: 5,
+        parent_id: 2
+      }]);
     })
     .then(() => {
       return knex.raw(
