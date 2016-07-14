63599763692b64d75ab99a3d5e3a402c0ae30b93
Hien Dang <hienpd@outlook.com>
2016-07-13 18:44:33 -0700

    Prepare the heroku

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,13 @@ module.exports = {
   },
 
   test: {
-      client: 'pg',
-      connection: 'postgres://localhost/family_tree_test'
+    client: 'pg',
+    connection: 'postgres://localhost/family_tree_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
+
 };
