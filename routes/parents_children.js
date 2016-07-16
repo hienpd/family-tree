39fb49e195bc234fbb31fd7393b55124937f3ea4
Michael Park <thatmichaelpark@gmail.com>
2016-07-16 10:27:17 -0700

    parents_children.js linted

--- routes/parents_children.js
+++ routes/parents_children.js
@@ -1,11 +1,9 @@
+/* eslint-disable camelcase */
 'use strict';
 
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // eslint-disable-line new-cap
 const knex = require('../knex');
-const server = require('../server');
-const ev = require('express-validation');
-const validations = require('../validations/parents_children');
 
 // POST /parents_children
 router.post('/parents_children', (req, res, next) => {
@@ -35,12 +33,10 @@ router.patch('/parents_children/:c_id', (req, res, next) => {
   knex('parents_children')
     .where('child_id', req.params.c_id)
     .del()
-    .then((res) => {
-      console.log('deleted', res);
-      console.log('req.body', req.body);
-      return knex('parents_children')
+    .then(() =>
+      knex('parents_children')
         .insert(req.body, '*')
-    })
+    )
     .then((parents_children) => {
       res.send(parents_children[0]);
     })
