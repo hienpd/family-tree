d6cde91b14403a4897a9e80d0b922b7f77c0bc49
Michael Park <thatmichaelpark@gmail.com>
2016-07-16 10:07:42 -0700

    email.js linted; still works

---
+++ routes/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'ryansobol/node',
+    'ryansobol/es6'
+  ]
+
+  // rules: {
+  //   'rule-name': 'off'  // 'error'
+  // }
+};
--- routes/email.js
+++ routes/email.js
@@ -1,33 +1,33 @@
 'use strict';
 
 const express = require('express');
-const router = express.Router();
+const router = express.Router();  // eslint-disable-line new-cap
 const rp = require('request-promise');
 
 router.post('/email', (req, res, next) => {
-  var options = {
-      method: 'POST',
-      uri: 'https://api.postmarkapp.com/email',
-      body: {
-          From: 'baobab@kenmcgrady.com',
-          To: req.body.email,
-          Subject: 'You\'ve been invited to Baobab!',
-          TextBody: 'Dude. Plz reply.'
-      },
-      headers: {
-        'X-Postmark-Server-Token': '76b40eca-7358-459b-8658-217bad4f9eb4'
+  const options = {
+    method: 'POST',
+    uri: 'https://api.postmarkapp.com/email',
+    body: {
+      From: 'baobab@kenmcgrady.com',
+      To: req.body.email,
+      Subject: 'You\'ve been invited to Baobab!',
+      TextBody: 'Dude. Plz reply.'
+    },
+    headers: {
+      'X-Postmark-Server-Token': '76b40eca-7358-459b-8658-217bad4f9eb4'
 
-      },
-      json: true // Automatically stringifies the body to JSON
+    },
+    json: true // Automatically stringifies the body to JSON
   };
 
   rp(options)
-      .then(function (parsedBody) {
-          res.sendStatus(200);
-      })
-      .catch(function (err) {
-          next(err);
-      });
-    });
+  .then(() => {
+    res.sendStatus(200);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
