aad2dfe790cead3fe34cff3d130566292f5a6e8c
Michael Park <thatmichaelpark@gmail.com>
2016-07-17 00:01:19 -0700

    people tests linted

--- test/routes.people.test.js
+++ test/routes.people.test.js
@@ -1,15 +1,17 @@
+/* globals before:false */
+/* globals beforeEach:false */
+/* eslint-disable quote-props */
 'use strict';
 
 process.env.NODE_ENV = 'test';
 
-const assert = require('chai').assert;
-const {suite, test} = require('mocha');
+const { suite, test } = require('mocha');
 const knex = require('../knex');
 const request = require('supertest');
 const server = require('../server');
 
 suite('people suite', () => {
-  before(function(done) {
+  before((done) => {
     knex.migrate.latest()
       .then(() => {
         done();
@@ -19,7 +21,7 @@ suite('people suite', () => {
       });
   });
 
-  beforeEach(function(done) {
+  beforeEach((done) => {
     knex.seed.run()
       .then(() => {
         done();
@@ -33,55 +35,55 @@ suite('people suite', () => {
     request(server)
       .get('/people')
       .expect(200, [{
-          "id": 1,
-          "given_name": "Bob",
-          "middle_name": "",
-          "family_name": "Belcher",
-          "dob": "1972-03-16T08:00:00.000Z",
-          "gender": "m",
-          "created_at": "2016-07-08T03:33:59.857Z",
-          "updated_at": "2016-07-08T03:33:59.857Z",
-          "user_id": null
+        "id": 1,
+        "given_name": "Bob",
+        "middle_name": "",
+        "family_name": "Belcher",
+        "dob": "1972-03-16T08:00:00.000Z",
+        "gender": "m",
+        "created_at": "2016-07-08T03:33:59.857Z",
+        "updated_at": "2016-07-08T03:33:59.857Z",
+        "user_id": null
       }, {
-          "id": 2,
-          "given_name": "Linda",
-          "middle_name": "",
-          "family_name": "Belcher",
-          "dob": "1972-07-24T07:00:00.000Z",
-          "gender": "f",
-          "created_at": "2016-07-08T03:33:59.857Z",
-          "updated_at": "2016-07-08T03:33:59.857Z",
-          "user_id": null
+        "id": 2,
+        "given_name": "Linda",
+        "middle_name": "",
+        "family_name": "Belcher",
+        "dob": "1972-07-24T07:00:00.000Z",
+        "gender": "f",
+        "created_at": "2016-07-08T03:33:59.857Z",
+        "updated_at": "2016-07-08T03:33:59.857Z",
+        "user_id": null
       }, {
-          "id": 3,
-          "given_name": "Tina",
-          "middle_name": "Ruth",
-          "family_name": "Belcher",
-          "dob": "2003-04-05T08:00:00.000Z",
-          "gender": "f",
-          "created_at": "2016-07-08T03:33:59.857Z",
-          "updated_at": "2016-07-08T03:33:59.857Z",
-          "user_id": 1
+        "id": 3,
+        "given_name": "Tina",
+        "middle_name": "Ruth",
+        "family_name": "Belcher",
+        "dob": "2003-04-05T08:00:00.000Z",
+        "gender": "f",
+        "created_at": "2016-07-08T03:33:59.857Z",
+        "updated_at": "2016-07-08T03:33:59.857Z",
+        "user_id": 1
       }, {
-          "id": 4,
-          "given_name": "Gene",
-          "middle_name": "",
-          "family_name": "Belcher",
-          "dob": "2005-04-05T07:00:00.000Z",
-          "gender": "m",
-          "created_at": "2016-07-08T03:33:59.857Z",
-          "updated_at": "2016-07-08T03:33:59.857Z",
-          "user_id": null
+        "id": 4,
+        "given_name": "Gene",
+        "middle_name": "",
+        "family_name": "Belcher",
+        "dob": "2005-04-05T07:00:00.000Z",
+        "gender": "m",
+        "created_at": "2016-07-08T03:33:59.857Z",
+        "updated_at": "2016-07-08T03:33:59.857Z",
+        "user_id": null
       }, {
-          "id": 5,
-          "given_name": "Louise",
-          "middle_name": "",
-          "family_name": "Belcher",
-          "dob": "2007-12-25T08:00:00.000Z",
-          "gender": "f",
-          "created_at": "2016-07-08T03:33:59.857Z",
-          "updated_at": "2016-07-08T03:33:59.857Z",
-          "user_id": null
+        "id": 5,
+        "given_name": "Louise",
+        "middle_name": "",
+        "family_name": "Belcher",
+        "dob": "2007-12-25T08:00:00.000Z",
+        "gender": "f",
+        "created_at": "2016-07-08T03:33:59.857Z",
+        "updated_at": "2016-07-08T03:33:59.857Z",
+        "user_id": null
       }], done);
   });
 
@@ -100,60 +102,61 @@ suite('people suite', () => {
         "updated_at": "2016-07-08T03:33:59.857Z",
         "user_id": null
       }, done);
-    });
-    test('GET people/15 route', (done) => {
-      request(server)
-      .get('/people/15')
-      .expect(404, done);
-    });
+  });
 
-    test('GET people/1/children route', (done) => {
-      request(server)
-      .get('/people/1/children')
-      .expect(200, [ { child_id: 3 }, { child_id: 4 }, { child_id: 5 } ], done);
-    });
+  test('GET people/15 route', (done) => {
+    request(server)
+    .get('/people/15')
+    .expect(404, done);
+  });
 
-    test('GET people/2/children route', (done) => {
-      request(server)
-      .get('/people/2/children')
-      .expect(200, [ { child_id: 3 }, { child_id: 4 }, { child_id: 5 } ], done);
-    });
+  test('GET people/1/children route', (done) => {
+    request(server)
+    .get('/people/1/children')
+    .expect(200, [{ child_id: 3 }, { child_id: 4 }, { child_id: 5 }], done);
+  });
 
-    test('GET people/1/parents route', (done) => {
-      request(server)
-      .get('/people/1/parents')
-      .expect(200, [], done);
-    });
+  test('GET people/2/children route', (done) => {
+    request(server)
+    .get('/people/2/children')
+    .expect(200, [{ child_id: 3 }, { child_id: 4 }, { child_id: 5 }], done);
+  });
 
-    test('GET people/5/parents route', (done) => {
-      request(server)
-      .get('/people/5/parents')
-      .expect(200, [{ parent_id: 1 }, { parent_id: 2 }], done);
-    });
+  test('GET people/1/parents route', (done) => {
+    request(server)
+    .get('/people/1/parents')
+    .expect(200, [], done);
+  });
 
-    test('POST /people', (done) => {
-      request(server)
-        .post('/people')
-        .send({
-          given_name: 'Tina',
-          middle_name: 'Ruth',
-          family_name: 'Belcher',
-          dob: '2003-04-05',
-          gender: 'f'
-        })
-        .expect('Content-Type', /json/)
-        .expect((res) => {
-          delete res.body.created_at;
-          delete res.body.updated_at;
-        })
-        .expect(200, {
-          id: 6,
-          given_name: 'Tina',
-          middle_name: 'Ruth',
-          family_name: 'Belcher',
-          dob: '2003-04-05T08:00:00.000Z',
-          gender: 'f',
-          user_id: null
-        }, done);
-    });
+  test('GET people/5/parents route', (done) => {
+    request(server)
+    .get('/people/5/parents')
+    .expect(200, [{ parent_id: 1 }, { parent_id: 2 }], done);
+  });
+
+  test('POST /people', (done) => {
+    request(server)
+      .post('/people')
+      .send({
+        given_name: 'Tina',
+        middle_name: 'Ruth',
+        family_name: 'Belcher',
+        dob: '2003-04-05',
+        gender: 'f'
+      })
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        delete res.body.created_at;
+        delete res.body.updated_at;
+      })
+      .expect(200, {
+        id: 6,
+        given_name: 'Tina',
+        middle_name: 'Ruth',
+        family_name: 'Belcher',
+        dob: '2003-04-05T08:00:00.000Z',
+        gender: 'f',
+        user_id: null
+      }, done);
+  });
 });
