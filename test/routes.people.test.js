f43d73b82410fae1fa763d52ffa617f18fe46834
Michael Park <thatmichaelpark@gmail.com>
2016-07-16 21:36:11 -0700

    people tests working

--- test/routes.people.test.js
+++ test/routes.people.test.js
@@ -33,165 +33,56 @@ suite('people suite', () => {
     request(server)
       .get('/people')
       .expect(200, [{
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": "1984-12-01T08:00:00.000Z",
-        "given_name": "Hien",
-        "gender": "f",
-        "id": 1,
-        "family_name": "Dang",
-        "middle_name": "Phuong",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": "1963-04-20T07:00:00.000Z",
-        "given_name": "Phung",
-        "gender": "f",
-        "id": 2,
-        "family_name": "Dang",
-        "middle_name": "Thi",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": "1995-03-05T08:00:00.000Z",
-        "given_name": "Nicholas",
-        "gender": "m",
-        "id": 3,
-        "family_name": "Nguyen",
-        "middle_name": "Hoai",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Thach",
-        "gender": "m",
-        "id": 4,
-        "family_name": "Nguyen",
-        "middle_name": "Van",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Lan",
-        "gender": "f",
-        "id": 5,
-        "family_name": "Nguyen",
-        "middle_name": "Thi",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Hoi",
-        "gender": "m",
-        "id": 6,
-        "family_name": "Dang",
-        "middle_name": "Van",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Nhung",
-        "gender": "f",
-        "id": 7,
-        "family_name": "Dang",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Hanh",
-        "gender": "f",
-        "id": 8,
-        "family_name": "Dang",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Chi",
-        "gender": "f",
-        "id": 9,
-        "family_name": "Dang",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Tu",
-        "gender": "f",
-        "id": 10,
-        "family_name": "Dang",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Phuc",
-        "gender": "f",
-        "id": 11,
-        "family_name": "Dang",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Thailand",
-        "gender": "f",
-        "id": 12,
-        "family_name": "Dang",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Ricky",
-        "gender": "m",
-        "id": 13,
-        "family_name": "Saeturn",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Sidney",
-        "gender": "f",
-        "id": 14,
-        "family_name": "Saeturn",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Lily",
-        "gender": "f",
-        "id": 15,
-        "family_name": "Saeturn",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    },
-    {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Nathan",
-        "gender": "m",
-        "id": 16,
-        "family_name": "Saeturn",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-    }], done);
+          "id": 1,
+          "given_name": "Bob",
+          "middle_name": "",
+          "family_name": "Belcher",
+          "dob": "1972-03-16T08:00:00.000Z",
+          "gender": "m",
+          "created_at": "2016-07-08T03:33:59.857Z",
+          "updated_at": "2016-07-08T03:33:59.857Z",
+          "user_id": null
+      }, {
+          "id": 2,
+          "given_name": "Linda",
+          "middle_name": "",
+          "family_name": "Belcher",
+          "dob": "1972-07-24T07:00:00.000Z",
+          "gender": "f",
+          "created_at": "2016-07-08T03:33:59.857Z",
+          "updated_at": "2016-07-08T03:33:59.857Z",
+          "user_id": null
+      }, {
+          "id": 3,
+          "given_name": "Tina",
+          "middle_name": "Ruth",
+          "family_name": "Belcher",
+          "dob": "2003-04-05T08:00:00.000Z",
+          "gender": "f",
+          "created_at": "2016-07-08T03:33:59.857Z",
+          "updated_at": "2016-07-08T03:33:59.857Z",
+          "user_id": 1
+      }, {
+          "id": 4,
+          "given_name": "Gene",
+          "middle_name": "",
+          "family_name": "Belcher",
+          "dob": "2005-04-05T07:00:00.000Z",
+          "gender": "m",
+          "created_at": "2016-07-08T03:33:59.857Z",
+          "updated_at": "2016-07-08T03:33:59.857Z",
+          "user_id": null
+      }, {
+          "id": 5,
+          "given_name": "Louise",
+          "middle_name": "",
+          "family_name": "Belcher",
+          "dob": "2007-12-25T08:00:00.000Z",
+          "gender": "f",
+          "created_at": "2016-07-08T03:33:59.857Z",
+          "updated_at": "2016-07-08T03:33:59.857Z",
+          "user_id": null
+      }], done);
   });
 
   test('GET people/2 route', (done) => {
@@ -199,59 +90,45 @@ suite('people suite', () => {
     .get('/people/2')
     .expect(200,
       {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": "1963-04-20T07:00:00.000Z",
-        "given_name": "Phung",
-        "gender": "f",
         "id": 2,
-        "family_name": "Dang",
-        "middle_name": "Thi",
-        "updated_at": "2016-07-08T03:33:59.857Z"
+        "given_name": "Linda",
+        "middle_name": "",
+        "family_name": "Belcher",
+        "dob": "1972-07-24T07:00:00.000Z",
+        "gender": "f",
+        "created_at": "2016-07-08T03:33:59.857Z",
+        "updated_at": "2016-07-08T03:33:59.857Z",
+        "user_id": null
       }, done);
     });
     test('GET people/15 route', (done) => {
       request(server)
       .get('/people/15')
-      .expect(200, {
-        "created_at": "2016-07-08T03:33:59.857Z",
-        "dob": null,
-        "given_name": "Lily",
-        "gender": "f",
-        "id": 15,
-        "family_name": "Saeturn",
-        "middle_name": "",
-        "updated_at": "2016-07-08T03:33:59.857Z"
-      }, done);
+      .expect(404, done);
     });
 
     test('GET people/1/children route', (done) => {
       request(server)
       .get('/people/1/children')
-      .expect(200, [], done);
+      .expect(200, [ { child_id: 3 }, { child_id: 4 }, { child_id: 5 } ], done);
     });
 
     test('GET people/2/children route', (done) => {
       request(server)
       .get('/people/2/children')
-      .expect(200, [{ child_id: 1}, { child_id: 3}], done);
+      .expect(200, [ { child_id: 3 }, { child_id: 4 }, { child_id: 5 } ], done);
     });
 
     test('GET people/1/parents route', (done) => {
       request(server)
       .get('/people/1/parents')
-      .expect(200, [{ parent_id: 2 }, { parent_id: 4 }], done);
-    });
-
-    test('GET people/2/parents route', (done) => {
-      request(server)
-      .get('/people/2/parents')
-      .expect(200, [{ parent_id: 5 }], done);
+      .expect(200, [], done);
     });
 
     test('GET people/5/parents route', (done) => {
       request(server)
       .get('/people/5/parents')
-      .expect(200, [], done);
+      .expect(200, [{ parent_id: 1 }, { parent_id: 2 }], done);
     });
 
     test('POST /people', (done) => {
@@ -270,12 +147,13 @@ suite('people suite', () => {
           delete res.body.updated_at;
         })
         .expect(200, {
-          id: 17,
+          id: 6,
           given_name: 'Tina',
           middle_name: 'Ruth',
           family_name: 'Belcher',
           dob: '2003-04-05T08:00:00.000Z',
-          gender: 'f'
+          gender: 'f',
+          user_id: null
         }, done);
     });
 });
